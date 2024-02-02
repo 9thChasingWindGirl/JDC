1f2dcc6b1f4eea4fb49604639efe46f32724326e
沢川千濑 <69580996+9thChasingWindGirl@users.noreply.github.com>
2024-02-02 18:14:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 # Test! Add a feed source ...
 echo 'src-git kenzo https://github.com/66o99/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/66o99/small' >>feeds.conf.default
-echo 'src-git packages https://git.openwrt.org/feed/packages.git^92a406deffcc03d58c2db7f115e0de145952db2b' >>feeds.conf.default
-echo 'src-git luci https://git.openwrt.org/project/luci.git^b6e968b5472bcff62f3410f1cfd92c199227ab15' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git^6f74738b725f8365c4c126b90f8f49f11e676720' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git^9e967bdde0ead47339ce7a037eda950ab3fa68a9' >>feeds.conf.default
+echo 'src-git packages https://git.openwrt.org/feed/packages.git' >>feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
